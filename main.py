139993a59cf121937075b498bb9df2cc780d876d
Christian Sada <95037926+Christiansada@users.noreply.github.com>
2024-12-09 13:29:00 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from langchain_helper import get_qa_chain
+
+st.title("Codebasics Q&A 🌱")
+
+
+question = st.text_input("Question: ")
+
+if question:
+    chain = get_qa_chain()
+    response = chain.invoke(question)
+
+    st.header("Answer")
+    st.write(response["result"])
+
+
+
+
+
+
